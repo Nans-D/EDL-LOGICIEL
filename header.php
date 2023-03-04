d7c6cef8ca5eee3611ee438fa2aed7860aceed4a
Nans-D <ndelaubert@gmail.com>
2023-03-04 14:03:10 +0100

    test

---
+++ header.php
@@ -0,0 +1,36 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>EDL LOGICIEL</title>
+</head>
+<body>
+    <header>
+    <img src="" alt="logo">
+            <p></p>
+            <input type="text" placeholder="Rechercher un dossier">
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <div>
+                <nav>
+                    <a id="" href="">infos du bien</a>
+                    <a id="" href="">type EDL</a>
+                    <a id="" href="">annexes</a>
+                    <a id="" href="">equip. ext.</a>
+                    <a id="" href="">equip. int.</a>
+                    <a id="" href="">prod. eau chaude</a>
+                    <a id="" href="">chauffage</a>
+                    <a id="" href="">relevés cpt. elec.</a>
+                    <a id="" href="">relevés cpt. gaz</a>
+                    <a id="" href="">remise clés</a>
+                    <a id="" href="">composition</a>
+                    <a id="" href="">conditions générales</a>
+                    <a id="" href="">identité</a>
+                    <a id="" href="">validation</a>
+                </nav>
+            </div>
+    </header>        
+</body>
